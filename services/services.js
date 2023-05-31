9a1218170675300a4dcfa737363cc9d9a9f41141
Elizabeth Escobar <eescoba5@correo.tdea.edu.co>
2023-05-30 19:51:48 -0500

    Primer commit

---
+++ services/services.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const apiUrl = 'https://www.themoviedb.org/3';
+const apiKey = 'api_key=756705f9f809e52f7567797ca0b3296a';
+
+// Get Popular Movies
+export const getPopularMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Upcoming Movies
+export const getUpcomingMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Popular TV
+export const getPopularTv = async () => {
+  const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Family Movies
+export const getFamilyMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=10751`,
+  );
+  return resp.data.results;
+};
+
+// Get Documnetery Movies
+export const getDocumentaryMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=99`,
+  );
+  return resp.data.results;
+};
+
+export const getMovie = async id => {
+  const resp = await axios.get(`${apiUrl}/movie/${id}?${apiKey}`);
+  return resp.data;
+};
+
+// Search for Movie or TV by Keyword
+export const searchMovieTv = async (query, type) => {
+  const resp = await axios.get(
+    `${apiUrl}/search/${type}?${apiKey}&query=${query}`,
+  );
+  return resp.data.results;
+};
